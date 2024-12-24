46cf58c81b5532e501f59c9e7843bb7dadb2d457
358543 <jlm156942@gmail.com>
2024-12-24 14:57:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll('.step h2').forEach((header) => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    content.style.display = content.style.display === 'none' ? 'block' : 'none';
+  });
+});
